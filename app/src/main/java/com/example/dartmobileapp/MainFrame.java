ac733843990a01b6b7476ed17488cff8e50b1ba5
d0h <dev1lc3st@gmail.com>
2025-02-11 09:26:18 +0300

    ъочу какатъ

---
+++ app/src/main/java/com/example/dartmobileapp/MainFrame.java
@@ -0,0 +1,41 @@
+package com.example.dartmobileapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainFrame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_frame);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Find the button by ID
+        Button logInButton = findViewById(R.id.loginButton);
+
+        // Set an OnClickListener for the button
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent to navigate to LogInActivity
+                Intent intent = new Intent(MainFrame.this, LogInActivity.class);
+                startActivity(intent); // Start the new activity
+            }
+        });
+    }
+
+}
